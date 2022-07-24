1f5fb6a1665891bc93ed9e79884b69312c001e6d
LuizHenriqueFN <luizhenriqueneves81@gmail.com>
2022-07-23 22:19:29 -0300

    CRUD_V02
    Nova tabela para exibir os dados e nova opção para editar os dados

--- luiz/src/main/java/com/crud/luiz/controller/CRUD_Controller.java
+++ luiz/src/main/java/com/crud/luiz/controller/CRUD_Controller.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.crud.luiz.model.Dados;
 
@@ -16,31 +18,50 @@ public class CRUD_Controller {
     List<Dados> Banco_de_Dados = new ArrayList<>();
 
     @GetMapping("/Index")
-    public String Pag_Inicial(){
-        return "index"; //Nome HTML
+    public ModelAndView Pag_Inicial(){
+        ModelAndView MV = new ModelAndView("index");
+        MV.addObject("Pessoa", new Dados());
+        return MV;
     }
 
     @PostMapping("/Index")
     public String Cadastra(Dados pessoa){
-        int id = Banco_de_Dados.size() + 1;
-        
-        Banco_de_Dados.add(new Dados(
-            id,
-            pessoa.getNome(),
-            pessoa.getApelido(),
-            pessoa.getTime(),
-            pessoa.getCPF(),
-            pessoa.getHobbie(),
-            pessoa.getCidade(),
-            pessoa.getQuantidade(),
-            pessoa.getEstado()
-        ));
+        if(pessoa.getId() != 0){
+            Dados Nova_Pessoa = Banco_de_Dados.stream().filter(P_Editando -> pessoa.getId() == P_Editando.getId()).findFirst().get();
+            
+            Banco_de_Dados.set(Banco_de_Dados.indexOf(Nova_Pessoa), pessoa);   
+        }else{
+            int id = Banco_de_Dados.size() + 1;
+            
+            Banco_de_Dados.add(new Dados(
+                id,
+                pessoa.getNome(),
+                pessoa.getApelido(),
+                pessoa.getTime(),
+                pessoa.getCPF(),
+                pessoa.getHobbie(),
+                pessoa.getCidade(),
+                pessoa.getQuantidade(),
+                pessoa.getEstado()
+            ));
+        }        
         return "redirect:/Tabela"; //Nome HTML
     }
 
     @GetMapping("/Tabela")
-    public String Tabela(){
-        return "Tabela";
+    public ModelAndView Tabela(){
+        ModelAndView MV = new ModelAndView("Tabela");
+        MV.addObject("Banco_De_Dados", Banco_de_Dados);
+        return MV;
+    }
+
+    @GetMapping("/Editar/{id}")
+    public ModelAndView Editar(@PathVariable("id") int id){
+        ModelAndView MV = new ModelAndView("index");
+
+        Dados Pessoa = Banco_de_Dados.stream().filter(Dados -> id == Dados.getId()).findFirst().get();
+        MV.addObject("Pessoa", Pessoa);
+        return MV;
     }
 }   
 
